bddc38ab8a3da7ff04af1f6003fce608b13640f6
firoz.anawar <firoz.rokiz@gmail.com>
2019-10-08 07:13:30 +0530

    Initial commit of RxJava

---
+++ app/src/main/java/com/firozanwar/rxjavabyfiroz/AsyncToRxActivity1.java
@@ -0,0 +1,58 @@
+package com.firozanwar.rxjavabyfiroz;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class AsyncToRxActivity1 extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1_async_to_rx);
+
+        progressBar = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.textView);
+
+        startAsyncTask("Firoz Anwar");
+    }
+
+    private void startAsyncTask(String input) {
+        Observable.just(input)
+                .map(this::doInBackground)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(this::onPreExecute)
+                .subscribe(this::onPostExecute);
+    }
+
+    private void onPreExecute() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    //do background things here
+    private int doInBackground(String data) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return data.length();
+    }
+
+    //post execution work here
+    private void onPostExecute(int result) {
+        progressBar.setVisibility(View.GONE);
+        textView.setText("Length of input is " + result);
+    }
+}
---
+++ app/src/main/java/com/firozanwar/rxjavabyfiroz/RetrofitWithRxActivity1.java
@@ -0,0 +1,14 @@
+package com.firozanwar.rxjavabyfiroz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RetrofitWithRxActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1_retrofit_with_rx);
+    }
+}
