f18b7e6997568050fb3843b8704841a3127a6ee4
inzimam.islam <inzimam.islam@timesinternet.in>
2019-10-08 16:13:59 +0530

    Timer example added

---
+++ app/src/main/java/com/firozanwar/rxjavabyfiroz/TimerActivity.java
@@ -0,0 +1,44 @@
+package com.firozanwar.rxjavabyfiroz;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
+public class TimerActivity extends AppCompatActivity {
+    Subscription subscription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+        startTimerTask();
+    }
+
+    private void startTimerTask() {
+        subscription = Observable.interval(3000, 3000, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        Toast.makeText(TimerActivity.this, "aLong : " + aLong, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (subscription != null)
+            subscription.unsubscribe();
+    }
+}
